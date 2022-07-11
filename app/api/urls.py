e1c74078022cf055ab743f3bee9346fa2ea20672
LeAries <learies@ya.ru>
2022-07-11 13:35:13 +0300

    Добавил отображение фотографий в альбомах

--- app/api/urls.py
+++ app/api/urls.py
@@ -11,6 +11,11 @@ router_v1 = DefaultRouter()
 router_v1.register('users', UserViewSet, 'user')
 router_v1.register('albums', AlbumViewSet, 'album')
 router_v1.register('photos', PhotoViewSet, 'photo')
+router_v1.register(
+    r'albums/(?P<album_id>\d+)/photos',
+    PhotoViewSet,
+    basename='photoalbum',
+)
 
 urlpatterns = (
     path('v1/', include(router_v1.urls)),
