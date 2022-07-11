7235c2ea54b7cc188d04e6ab437183c17cbd07c4
LeAries <learies@ya.ru>
2022-07-11 12:14:45 +0300

    Добавил шаблон redoc

--- app/photo_album/settings.py
+++ app/photo_album/settings.py
@@ -43,10 +43,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'photo_album.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': (TEMPLATES_DIR,),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
